37beff681ea399e25642a53406c87b0fd20b12c3
meryla87 <marionce@gmail.com>
2022-04-13 09:53:17 -0300

    Comienzo d eproyecto en VSC para github

---
+++ prueba VSC_github.py
@@ -0,0 +1,8 @@
+print('Empezando las pruebas para trabajabar con el repo de github')
+
+l = [1,2,3,4,5,6,7,8,9]
+for i in l:
+    if i%2==0:
+        print(str(i)+' es par')
+    else:
+        print(str(i)+' es impar')
